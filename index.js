6607920c3f76c96cd088ea8776a81404b572be7e
ac-89 <alancarroll89@gmail.com>
2023-12-12 12:08:03 +0000

    rename files

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const express = require("express")
+const app = express();
+
+const PORT = 3000
+
+const rappers = {
+"21 savage":{
+    "age": 29,
+    "birthName": "Sheyaa",
+    "birthLocation": "London, UK",
+}, "chance the rapper":{
+    "age": 28,
+    "birthName": "Chancelor Bennett",
+    "birthLocation": "Chicago, Illinois",
+},"50 cent":{
+    "age": 48,
+    "birthName": "Curtis James Jackson III",
+    "birthLocation": "New York City, U.S.",
+}  
+}
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.get(`/api`, (req, res) => {
+    res.json(rappers)
+})
+
+app.get(`/api/:rapperName`, (req, res) =>{
+    const rappersName = req.params.rapperName.toLowerCase()
+    if(rappers[rappersName]){
+        res.json(rappers[rappersName])
+    }else{
+        res.json("Bleh")
+    }
+})
+
+app.listen(process.env.PORT || PORT, () =>{
+    console.log("Hello...")
+})

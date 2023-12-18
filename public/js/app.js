d3ba66b4a47f06177989d94a6a1d5cb091d6d8a8
ac-89 <alancarroll89@gmail.com>
2023-12-18 11:03:23 +0000

    Additiona fields

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,9 @@ async function apiRequest(){
         const data = await response.json()
 
         console.log(data)
-        document.querySelector('h2').innerText = data.birthName
+        document.querySelector('#birthName').innerText = data.birthName
+        document.querySelector('#age').innerText = data.age
+        document.querySelector('#birthPlace').innerText = data.birthPlace
     }catch(error){
         console.log(error)
     }
